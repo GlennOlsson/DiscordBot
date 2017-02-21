079a34c30bbd966b5ad691f200ca0cc6defab3f4
Glenn <gojbmail@gmail.com>
2017-02-21 14:20:20 +0100

    Testing new way to parse links, so NSFW posts works
    Signed-off-by: Glenn <gojbmail@gmail.com>

--- Discord/src/Test2.java
+++ Discord/src/Test2.java
@@ -1,3 +1,6 @@
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 public class Test2 {
@@ -5,12 +8,28 @@ public class Test2 {
 	public static void main(String[] args) {
 		// FIXME Auto-generated method stub
 
-		Document doc;
+		String url = "https://www.reddit.com/r/ImGoingToHellForThis/comments/5v9667/hate_when_this_happens/";
+		
+		Document doc=null;
 		//		try {
 		//			doc = Jsoup.connect("http://www.reddit.com/r/shittyrainbow6/comments/5uon69/best_tactic_for_blitz/").userAgent("Chrome").get();
 		//			System.out.println(doc.select("#media-preview-5uon69 > div > a").attr("href"));
 		//		} catch (Exception e) {
 		//			e.printStackTrace();
 		//	}
+		try {
+			doc = Jsoup.connect(url+".rss").userAgent("Chrome").get();
+		} catch (IOException e) {
+			// FIXME Auto-generated catch block
+			e.printStackTrace();
+		}
+		String docString = doc.toString();
+		
+		docString=docString.substring(docString.indexOf("span&gt;&lt;a href=")+"span&gt;&lt;a href=".length()+1,
+				docString.indexOf("&gt;[link]&lt;/a&gt;&lt;")-1).replaceAll("amp;amp;", "");
+		
+//		docString=docString.replaceAll("amp;amp;", "");
+		
+		System.out.println(docString);
 	}
 }
