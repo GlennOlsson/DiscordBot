3df14db946cf279b7d659c110132e075df54e5ee
Glenn <gojbmail@gmail.com>
2017-02-24 01:04:52 +0100

    Added ;clean [1-100] [bots, users, all]
    Fixed so it looks better --> All commands and features has their own
    function that is called from the main function
    
    Signed-off-by: Glenn <gojbmail@gmail.com>

--- Discord/src/Test2.java
+++ Discord/src/Test2.java
@@ -1,10 +1,14 @@
 
+import java.util.List;
+
 import org.jsoup.nodes.*;
 
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.MessageHistory;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.Role;
 import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
@@ -47,35 +51,92 @@ public class Test2 extends ListenerAdapter{
 			TextChannel channel=event.getTextChannel();
 			String content = event.getMessage().getContent().toLowerCase();
 
-			if(event.getMessage().getContent().toString().contains("!clean")&&content.substring(0,"!clean".length()).equals("!clean")){
-				int amount;
-				if(content.contains(" ")){
-					try {
-						amount=Integer.parseInt(content.split(" ")[1]);
-					} catch (Exception e) {
-						// FIXME: handle exception
-						event.getAuthor().getPrivateChannel().sendMessage("Error in argument");
-					}
-				}else{
-					event.getAuthor().getPrivateChannel().sendMessage("No amount specified, deleting one");
-					amount=1;
-				}
-				
-				MessageHistory history = channel.getHistory();
-				channel.getHistoryAround("!clean", 10).queue();
-				history.retrievePast(10).queue();
-				
-				String[] roles ={"Moderator", "Commissioner", "Server Owner"};
-				
-				System.err.println("CLEAN");
-				System.out.println(" -- HE");
-				for (int i = 0; i < channel.getMembers().size(); i++) {
-
+			if(event.getMessage().getContent().toString().contains(";clean")&&content.substring(0,";clean".length()).equals(";clean")){
+				List<Role> roles = null;
+				for (int i = 0; i < channel.getMembers().size(); i++) {		
 					if(channel.getMembers().get(i).getUser()==event.getAuthor()){
-						System.out.println(channel.getMembers().get(i).getRoles());
-						System.out.println(event.getAuthor());
+						roles =channel.getMembers().get(i).getRoles();
+						i= channel.getMembers().size()+5;
 					}
-					//			System.out.println(channel.getPermissionOverride();
+				}
+
+				String[] roleslist ={"Moderator", "Commissioner", "Server Owner"};
+				Boolean permission=false;
+				for (int i = 0; i < roles.size(); i++) {
+					if(roles.get(i).getName().toLowerCase().equals("moderator")||roles.get(i).getName().toLowerCase().equals("commissioner")||
+							roles.get(i).getName().toLowerCase().equals("server owner")){
+						permission=true;
+						i=roles.size()+5;
+					}
+				}
+				if(permission){
+					int amount = 1;
+					String argument="all";
+					if(content.contains(" ")){
+						try {
+							amount=Integer.parseInt(content.split(" ")[1]);
+							if (amount>100) {
+								amount=100;
+							}
+						} catch (Exception e) {
+							// FIXME: handle exception
+							event.getAuthor().getPrivateChannel().sendMessage("Error in argument, deleting one").queue();;
+						}
+						if(content.split(" ").length==3){
+							if(content.split(" ")[2].toLowerCase().equals("bots")||content.split(" ")[2].toLowerCase().equals("users")||
+									content.split(" ")[2].toLowerCase().equals("all")){
+								argument=content.split(" ")[2].toLowerCase();
+							}
+
+						}
+						if(content.split(" ").length>=4){
+							channel.sendMessage("To many arguments *"+event.getAuthor().getAsMention() +"*, aborting command").queue();
+							return;
+						}
+					}
+					else{
+//						event.getAuthor().getPrivateChannel().sendMessage("No amount specified, deleting one").queue();;
+					}
+
+					//				channel.getHistory().
+
+					MessageHistory history = channel.getHistory();
+					channel.getHistory().retrievePast(100).queue();
+					List<Message> historyList = null;
+					try {
+						historyList=history.retrievePast(100).block();
+					} catch (Exception e) {
+						// FIXME Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+					for (int i = 0; i < amount; i++) {
+						if(argument.equals("bots")){
+							if(historyList.get(i).getAuthor().isBot()){
+								historyList.get(i).deleteMessage().queue();	
+							}
+							else{
+								amount++;
+							}
+						}
+						else if (argument.equals("users")) {
+							if(!historyList.get(i).getAuthor().isBot()){
+								historyList.get(i).deleteMessage().queue();	
+							}
+							else{
+								amount++;
+							}
+						}
+						else {
+							//if all user's messages shall go
+							historyList.get(i).deleteMessage().queue();	
+						}
+					}
+					System.err.println(amount+" = amount");
+				}
+				else {
+//					channel.sendMessage("You are not authorized to execute that command, *"+event.getAuthor().getAsMention()+"*. Contact a Moderator").queue();
+					return;
 				}
 			}
 		}
