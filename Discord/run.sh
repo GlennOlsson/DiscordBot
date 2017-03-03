86435b1572e4d38d96c8de77cdf857ba2beb82c3
Glenn <gojbmail@gmail.com>
2017-03-03 15:28:51 +0100

    Updated JDA jars
    108 --> 143
    
    Signed-off-by: Glenn <gojbmail@gmail.com>

--- Discord/run.sh
+++ Discord/run.sh
@@ -1,1 +1,1 @@
-java -cp bin:JDA-3.0.BETA2_108.jar:JDA-3.0.BETA2_108-withDependencies.jar:jsoup-1.8.1.jar DiscordBot
+java -cp bin:JDA-3.0.BETA2_143.jar:JDA-3.0.BETA2_143-withDependencies.jar:jsoup-1.8.1.jar DiscordBot
--- Discord/src/Test.java
+++ Discord/src/Test.java
@@ -1,11 +1,20 @@
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import org.jsoup.nodes.*;
 
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.Game;
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.PrivateChannel;
 import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.events.message.priv.PrivateMessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 public class Test extends ListenerAdapter{
@@ -30,14 +39,105 @@ public class Test extends ListenerAdapter{
 			e.printStackTrace();
 		}
 
-		TextChannel channel=jda.getGuildsByName("Kakanistan",true).get(0).getTextChannels().get(0);
+		TextChannel channels=jda.getGuildsByName("Kakanistan",true).get(0).getTextChannels().get(0);
+
+		for (Member string : channels.getMembers()) {
+			if(string.getUser().getName().toLowerCase().equals("kakan")){
+				System.err.println(string.getUser());
+			}
+		}
+
+		Game game = Game.of("Hayy!");
+		
+		jda.getPresence().setGame(game);
+		
+		//		System.out.println(jda.getShardInfo());
 
 	}
 
 	public Test(){
 
 	}
-	public void onPrivateMessageReceived(MessageReceivedEvent event){
+	public void onMessageReceived(MessageReceivedEvent event){	
+		String content = event.getMessage().getContent().toLowerCase();
+
+		if(content.contains(";sup")&&content.substring(0, ";sup".length()).equals(";sup")){
+			PrivateChannel privateChannel=null;
+			
+			if(event.getAuthor().hasPrivateChannel()){
+				privateChannel=event.getAuthor().getPrivateChannel();
+			}
+			else {
+				event.getAuthor().openPrivateChannel().queue();
+				privateChannel=event.getAuthor().getPrivateChannel();
+			}
+			
+			privateChannel.sendMessage("Sup").queue();
+			
+			
+			
+		}
+	}
+	public void onPrivateMessageReceived(PrivateMessageReceivedEvent event){
+
+		String content = event.getMessage().getContent().toLowerCase();
+
+		if(content.contains(";sup")&&content.substring(0, ";sup".length()).equals(";sup")){
+			System.out.println();
+		}
+
+		if(content.contains(";send")&&content.substring(0, ";send".length()).equals(";send")){
+			String user= event.getAuthor().getName().toLowerCase() + "#"+event.getAuthor().getDiscriminator().toLowerCase();
+			if (user.equals("kakan#2926")) {
+				System.err.println("AUTHORIZED");
+				if(!content.contains(" ")){
+					event.getChannel().sendMessage("-- YOU CAN MESSAGE THE FOLLOWING CHANNELS --").queue();
+					List<TextChannel> channels = event.getJDA().getTextChannels();
+
+					List<String> channelNames=new ArrayList<>();
+
+					for (TextChannel channel : event.getJDA().getTextChannels()) {
+						channelNames.add(channel.getGuild().getName() +" - "+ channel.getName());
+
+					}
+
+					Collections.sort(channelNames, new Comparator<String>() {
+						@Override
+						public int compare(String s1, String s2) {
+							return s1.compareToIgnoreCase(s2);
+						}
+					});
+					int i=0;
+					for (String string : channelNames) {
+						i++;
+						event.getChannel().sendMessage(i+". "+string).queue();
+					}
+
+					event.getChannel().sendMessage("-- THAT IS ALL --").queue();
+				}
+				else if (content.split(" ").length==2) {
+					String arg = content.split(" ")[1];
+
+					List<TextChannel> channels = event.getJDA().getTextChannels();
+					List<String> channelNames=new ArrayList<>();
+
+					for (TextChannel channel : event.getJDA().getTextChannels()) {
+						channelNames.add(channel.getGuild().getName() +" - "+ channel.getName());
+
+					}
+
+					Collections.sort(channelNames, new Comparator<String>() {
+						@Override
+						public int compare(String s1, String s2) {
+							return s1.compareToIgnoreCase(s2);
+						}
+					});
+
+					System.out.println(channelNames.get(Integer.parseInt(arg)-1));
+
+				}
+			}
+		}
 
 	}
 }
