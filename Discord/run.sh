ec631e2576fc5c92e6445a20ca440befe74e85ae
Kakan9898 <glennholsson@gmail.com>
2017-03-27 14:23:54 +0200

    Merge no good, fuck that
    Signed-off-by: Kakan9898 <glennholsson@gmail.com>

--- Discord/run.sh
+++ Discord/run.sh
@@ -1,3 +1,1 @@
-javac -d bin -sourcepath src -cp JDA-3.0.BETA2_143.jar:JDA-3.0.BETA2_143-withDependencies.jar:jsoup-1.8.1.jar:json-simple-1.1.1.jar:jsch-0.1.54.jar:org.eclipse.jgit-4.6.0.201612231935-r.jar:slf4j-api-1.7.24.jar src/main/DiscordBot.java
-echo "Compile success, now running"
 java -cp bin:JDA-3.0.BETA2_143.jar:JDA-3.0.BETA2_143-withDependencies.jar:jsoup-1.8.1.jar:json-simple-1.1.1.jar:json-simple-1.1.1.jar:jsch-0.1.54.jar:org.eclipse.jgit-4.6.0.201612231935-r.jar:slf4j-api-1.7.24.jar main/DiscordBot
