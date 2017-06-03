b25449f32743efec3d82bfb787ef33efb99c7dcf
Glenn <glennholsson@gmail.com>
2017-06-03 20:48:18 +0200

    Moved all .jar to lib directory. Updated compile.sh to test using all .jar in the lib folder, instead of manually adding them

--- Discord/compile.sh
+++ Discord/compile.sh
@@ -1,1 +1,2 @@
-javac -d bin -sourcepath src -cp JDA-3.0.BETA2_143.jar:JDA-3.0.BETA2_143-withDependencies.jar:jsoup-1.8.1.jar:json-simple-1.1.1.jar:jsch-0.1.54.jar:org.eclipse.jgit-4.6.0.201612231935-r.jar:slf4j-api-1.7.24.jar src/main/DiscordBot.java
+#!/usr/bin/env bash
+javac -d bin -sourcepath src -cp "lib/*" src.main.DiscordBot.java
