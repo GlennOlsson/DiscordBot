59112308a5366118ba6976b446b3d93903f8c001
Glenn <glennholsson@gmail.com>
2017-06-04 11:27:09 +0200

    Addded echo after every line, fixed tmux send keys

--- Discord/restart.sh
+++ Discord/restart.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 cd /home/pi/DiscordBot/DiscordBot/Discord || { echo 'cd failed' ; exit 1; }
+echo 'managed to cd'
 git pull || { echo 'git pull failed' ; exit 1; }
+echo 'managed to git pull'
 sh compile.sh || { echo 'sh compile.sh failed' ; exit 1; }
-tmux send-keys -t nvim Escape || { echo 'Could not exit running script in tmux'; exit 1; }
+echo 'managed to compile'
+tmux send-keys -t 0 ^C || { echo 'Could not exit running script in tmux'; exit 1; }
+echo 'managed to exit running tmux script'
 tmux attach || { echo 'tmux attach failed';}
+echo 'managed to attach'
 sh rum.sh || { echo 'Could not run'; exit 1; }
+echo 'managed to run'
 echo 'Successfully reset DiscordBot'
