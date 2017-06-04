ad7e5bf57bedfe2b304e842345c2ed96de59ade6
Glenn <glennholsson@gmail.com>
2017-06-04 11:34:17 +0200

    More updates to restart.sh

--- Discord/restart.sh
+++ Discord/restart.sh
@@ -7,7 +7,7 @@ sh compile.sh || { echo 'sh compile.sh failed' ; exit 1; }
 echo 'managed to compile'
 tmux send-keys -t 0 ^C || { echo 'Could not exit running script in tmux'; exit 1; }
 echo 'managed to exit running tmux script'
-tmux send-keys -t 0 sh run.sh Enter || { echo 'Could not run'; exit 1; }
+tmux send-keys -t 0 'sh run.sh' Enter || { echo 'Could not run'; exit 1; }
 echo 'managed send run and execute'
 #tmux send-keys -t 0 Enter|| { echo 'Could not run'; exit 1; }
 echo 'Successfully reset DiscordBot'
