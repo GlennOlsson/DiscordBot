a86ea1ae6d0aed2a3e512f0926398422dc94c70a
Glenn Olsson <glennol@kth.se>
2017-09-28 20:29:35 +0200

    Added sleep to restar.sh

--- restart.sh
+++ restart.sh
@@ -5,7 +5,9 @@ echo 'Managed to cd, trying to git pull'
 git pull || { echo 'git pull failed' ; exit 1; }
 echo 'Managed to git pull, trying to exit running tmux'
 tmux send-keys -t 0 ^C || { echo 'Could not exit running script in tmux'; exit 1; }
-echo 'Managed to exit running tmux script, trying to run and execute'
+echo 'Managed to exit running tmux script, trying to sleep for 5 seconds'
+sleep 5
+echo 'Done sleeping, trying to run and execute'
 tmux send-keys -t 0 'gradle run' Enter || { echo 'Could not run'; exit 1; }
 echo 'Managed send run and execute'
 #tmux send-keys -t 0 Enter|| { echo 'Could not run'; exit 1; }
