9810e51c68b02466e10e56c24e2484559ac5560e
Glenn Olsson <glennol@kth.se>
2018-01-02 23:49:20 +0100

    Tmux will now start with a fixed name of DiscordBot, so they can be easier attached to. Both restart.sh and start.sh has been updated with this

--- restart.sh
+++ restart.sh
@@ -4,11 +4,11 @@ cd /home/tau/DiscordBot/DiscordBot || { echo 'cd failed' ; exit 1; }
 echo 'Managed to cd, trying to git pull'
 git pull || { echo 'git pull failed' ; exit 1; }
 echo 'Managed to git pull, trying to exit running tmux'
-tmux send-keys -t 0 ^C || { echo 'Could not exit running script in tmux'; exit 1; }
+tmux send-keys -t DiscordBot ^C || { echo 'Could not exit running script in tmux'; exit 1; }
 echo 'Managed to exit running tmux script, trying to sleep for 5 seconds'
 sleep 5
 echo 'Done sleeping, trying to run and execute'
-tmux send-keys -t 0 'gradle run' Enter || { echo 'Could not run'; exit 1; }
+tmux send-keys -t DiscordBot 'gradle run' Enter || { echo 'Could not run'; exit 1; }
 echo 'Managed send run and execute'
 #tmux send-keys -t 0 Enter|| { echo 'Could not run'; exit 1; }
 echo 'Successfully reset DiscordBot'
